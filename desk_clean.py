0d94817933bf784906b00eaa8cdfd4e668ecbdb3
Dan <clarc.2007@gmail.com>
2024-02-07 15:42:48 +0200

    Created the check_desktop function to check if on desktop there are files and folder needed to be organized

--- desk_clean.py
+++ desk_clean.py
@@ -6,3 +6,16 @@ user = os.getlogin()
 path = f"C:\\Users\\{user}\\Desktop\\"
 files_folder = "Desktop_Data_Files"
 folders_folder = "Desktop_Data_Folders"
+
+def check_desktop():
+    """
+       The function checks if on desktop there are files
+       or folders that needs to be organized.
+    """
+
+    desktop = os.listdir(path)
+    if desktop != []:
+        for item in desktop:
+            if item != files_folder and item != folders_folder and "lnk" not in item:
+                return desktop
+    return False
