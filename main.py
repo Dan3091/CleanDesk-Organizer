334d2c988b1c192f2e9f4157d633ad217c81f6dc
Dan <clarc.2007@gmail.com>
2024-02-07 16:50:15 +0200

    Added the doc string for all functions, and added the istructions for check_pc_health and change_title functions

--- main.py
+++ main.py
@@ -9,20 +9,54 @@ import time
 import threading
 
 def popup_window(message):
+    """
+    The function take a message as argument and create a popup window.
+    """
+
     pass
+
 def check_pc_health():
-    pass
+    """
+    This function by using psutil and shutil modules return the cpu usage,
+    ram usage and free disk space in % in a tuple format.
+    """
+
+    cpu = psutil.cpu_percent(interval=0.5, percpu=False)
+    ram = psutil.virtual_memory()[2]
+    disk = round(shutil.disk_usage("C:")[2] / shutil.disk_usage("C:")[0] * 100)
+    return cpu, ram, disk
 
 def change_title():
-    pass
+    """
+    The function enter in a while loop and checks every 1 second the cpu usage,
+    ram usage and free disk space and assign to (icon.title) object.
+    """
+
+    while True:
+        icon.title = f"CPU: {check_pc_health()[0]}%\n" + \
+                     f"RAM: {check_pc_health()[1]}%\n" + \
+                     f"DISK: {check_pc_health()[2]}%"
+        time.sleep(1)
 
 def check_desktop_status():
+    """
+    The function checks automatically every 30 minutes, if on desktop there are
+    more than 10 folders and files then it call a popup window.
+    """
+
     pass
 
 def optimizer(icon, item):
+    """
+    This function is a part of system tray menu it calls the clean_desktop function.
+    """
     clean_desktop()
 
 def quit_window(icon, item):
+    """
+    This function is a part of system tray menu it calls the stop() function from icon object.
+    """
+
     icon.stop()
 
 if __name__ == "__main__":
