3c3eea74affb6b605b7ff7030534b3933aa3c330
Dan <clarc.2007@gmail.com>
2024-02-07 15:50:27 +0200

    Created the folder_organizer function to organize folders from Desktop inside Desktop_Data_Folders

--- desk_clean.py
+++ desk_clean.py
@@ -19,3 +19,30 @@ def check_desktop():
             if item != files_folder and item != folders_folder and "lnk" not in item:
                 return desktop
     return False
+
+
+def folder_organizer(item):
+    """
+       The function takes the name of a folder as argument,
+       first it checks if the folder is not empty then
+       it tries to create the main folder(Desktop_Data_Folders),
+       checks if there isn't any folder with the same name,
+       move it to the main folder on desktop(Desktop_Data_Folders),
+       otherwise move it and change the name of the folder.
+    """
+
+    if os.listdir(path + item):
+        try:
+            os.mkdir(os.path.join(path, folders_folder))
+        except:
+            pass
+        if item not in os.listdir(path + folders_folder + "\\"):
+            os.rename(path + item, path + folders_folder + "\\" + item)
+        else:
+            while True:
+                generated_number = str(random.randint(1000, 9999))
+                if item + generated_number not in os.listdir(path + folders_folder + "\\"):
+                    os.rename(path + item, path + folders_folder + "\\" + item + generated_number)
+                    break
+    else:
+        os.rmdir(path + item)
