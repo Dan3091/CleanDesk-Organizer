a25be9fd1471bc7a386156e77690bc2bad5e2258
Dan <clarc.2007@gmail.com>
2024-02-07 16:14:25 +0200

    Imported all necessary modules

---
+++ main.py
@@ -0,0 +1,9 @@
+from tkinter import *
+import pystray
+from PIL import Image
+from pystray import MenuItem as item
+from desk_clean import *
+import psutil
+import shutil
+import time
+import threading
